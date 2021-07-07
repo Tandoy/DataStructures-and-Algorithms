87751291da3d1bdad14d40d6f0eef18b1878fcda
Tandoy <tangzhi1318@163.com>
2021-07-07 13:05:04 +0800

    Algorithms：希尔排序代码实现之移位法

--- Algorithms/Algorithms_code/src/main/java/com/tz/sort/ShellSort.java
+++ Algorithms/Algorithms_code/src/main/java/com/tz/sort/ShellSort.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class ShellSort {
     public static void main(String[] args) {
         int[] arr1 = new int[]{8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
-        shellSort(arr1);
+        shellSort1(arr1);
         System.out.println(Arrays.toString(arr1));
 
         // 创建要给80000个的随机的数组
@@ -24,13 +24,13 @@ public class ShellSort {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String date1Str = simpleDateFormat.format(data1);
         System.out.println("排序前的时间是=" + date1Str);
-        shellSort(arr); //调用插入排序算法
+        shellSort1(arr); //调用插入排序算法
         Date data2 = new Date();
         String date2Str = simpleDateFormat.format(data2);
         System.out.println("排序后的时间是=" + date2Str);
     }
 
-    // 希尔排序
+    // 希尔排序-->交换法
     public static void shellSort(int[] arr) {
         int temp = 0;
         for (int grap = arr.length / 2; grap > 0; grap /= 2) {
@@ -85,5 +85,25 @@ public class ShellSort {
         }
         System.out.println("第3轮后数组："+Arrays.toString(arr));
  */
+    }
+
+    // 希尔排序-->移位法
+    public static void shellSort1(int[] arr) {
+        for (int grap = arr.length / 2; grap > 0; grap /= 2) {
+            // 从第 grap 个元素，逐个对其所在的组进行直接插入排序
+            for (int i = grap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - grap]) {
+                    while (j - grap >= 0 && temp < arr[j - grap]) {
+                        //移动
+                        arr[j] = arr[j - grap];
+                        j -= grap;
+                    }
+                    //当退出 while 后，就给 temp 找到插入的位置
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
